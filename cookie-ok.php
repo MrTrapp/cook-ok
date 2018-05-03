14f6472b7b69f6acf8985f2f6d371097951cb250
MrTrapp <github@trapp-design.eu>
2018-05-03 10:03:24 +0200

    Update cookie-ok.php

--- cookie-ok.php
+++ cookie-ok.php
@@ -2,16 +2,24 @@
 // Infos
 // https://www.php-einfach.de/php-tutorial/php-sessions/#Ueberpruefen_ob_Session-Variable_registriert_ist 
 // http://php.net/manual/de/function.setcookie.php
+// https://cookieconsent.insites.com/download/
+// ab REDAXO 5.X https://github.com/FriendsOfREDAXO/cookie_consent
+// REX_TEMPLATE[X]   // Cookies-OK
 
+//Tipp zum einbetten von Google-Fonts
+// https://google-webfonts-helper.herokuapp.com/fonts/give-you-glory?subsets=latin
 $info = '';
 $style = '';
 $PHP_SELF = '';
-$readClosed = '';
+$readClosed = ''; 
+
 
 // !!! Nicht vergessen den Link zur Datenschutzerklährung anpassen. !!! \\\
 $pfad = '/'; // '.$PHP_SELF.'
 $pfadRex4 = 'index.php?article_id=1'; // " . $_SERVER[".$PHP_SELF."] . "
 $pfadRex5 = rex_getUrl(1); // .rex::getServer().$PHP_SELF.'
+$pfadModal = '#" class="x" data-toggle="modal" data-target="#datenschutz';
+
 
 // Info Text mit Button über Cookie 
 $info = '<form action="'.$PHP_SELF.'" method="post">
@@ -27,34 +35,61 @@ Privacy Policy';
 
 // Aktions abhängige Styles
 $style = '<style>
-.container {
-	}
+
 </style>';
 
 
 // Wenn der Button OK geklickt worden ist:
-// $readClosed = $_POST["read_and_close"];
 $readClosed = isset($_POST["read_and_close"]);
 
 
 if ($readClosed == 'OK') {
 	// Setzen des Cookies => cookies-ok 
-	setcookie ("cookiesok","cookies-ok");// ("cookie[ok]", "cookies-ok");
+	setcookie("cookiesok","cookies-ok", time()+60*60*24*30); // time für einen Monat
 	$info = ''; 
 	$style = '';
 }
-// Wenn das Cookie cookies-ok gesetzt wurde, Indo heraus nehmen
+// Wenn das Cookie cookies-ok gesetzt wurde, Info heraus nehmen
 if (isset($_COOKIE['cookiesok']) == true ) {
 	$info = '';
 	$style = '';
 }
 // AUSGABE
 echo $info.$style;
+
 /*
 // Test 
 echo '<br><br><h1>Button click : '.$readClosed.' <br> COOKIE: '.htmlspecialchars($_COOKIE["cookiesok"]).'</h1>';
 echo $_COOKIE['cookie-ok'];
 */
+
+/*
+<!-- Modal für REX 5 und Bootstrap 4 -->
+<div id="datenschutz" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Datenschutz</h4>
+      </div><!-- modal-header //-->
+      <div class="modal-body">
+	  <?php
+		  $footer1 = new rex_article_content(14);
+		  echo $footer1->getArticle(); // ->getArticle(1)
+	  
+	  ?>
+      </div><!-- modal-body  //-->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div><!-- modal-footer  //-->
+    </div><!-- modal-content  //-->
+
+  </div><!-- modal-dialog //-->
+</div><!--  modal fade //-->
+*/
+
 // eigene Styles
 ?>
 <style>
